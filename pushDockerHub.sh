d2818487bddee1f10e305a2ee9652b6f9097bba4
pooja2012 <33168116+pooja2012@users.noreply.github.com>
2017-12-15 11:52:08 +0000

    Add files via upload

---
+++ pushDockerHub.sh
@@ -0,0 +1,5 @@
+echo "push to dockerhub"
+docker build --build-arg CACHEBUST=$(date +%s) .
+docker images
+echo "docker tag <tag> pooja2012/CodeComplexity"
+echo "docker push pooja2012/CodeComplexity"
